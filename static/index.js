124d372bcc9b0200da233eb61a50d383c6d56bb9
AncientSoup <craftydaman7@gmail.com>
2022-04-29 22:01:23 +0800

    first commit

---
+++ static/index.js
@@ -0,0 +1,32 @@
+// Collapse nav bar on click (mobile view)
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {
+  toggle: false
+})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+
+// Show 'scroll back to top' button after a certain amount of scrolling '
+document.addEventListener('scroll', () => {
+    scrollVisibility();
+})
+
+const scroll = document.querySelector('.scrollup');
+
+scroll.addEventListener('click', () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
+
+const scrollVisibility = () => {
+    if (window.scrollY > 400) {
+        scroll.style.visibility = 'visible';
+    }
+    else {
+        scroll.style.visibility = 'hidden';
+    }
+}
